3e0e3f1fcbe55c71daa279e3b2192ca6e2a87447
GingerLutterium <117631451+GingerLutterium@users.noreply.github.com>
2022-11-07 17:09:17 +0200

    Add files via upload

---
+++ CSharp/WithoutCustomDLLImport/GameWerSuspenderLib-CSharpEdition/GameWerSuspend.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameWerSuspenderLib_CSharpEdition
+{
+    public class GameWerSuspend
+    {
+        [DllImport("ntdll.dll", PreserveSig = false)]
+        public static extern void NtSuspendProcess(IntPtr processHandle);
+        private IntPtr GetHandleGameWerProcess()
+        {
+            IntPtr int_ptr = IntPtr.Zero;
+            Process[] gamewer = Process.GetProcessesByName("Alkad.exe");
+            foreach(Process proc in gamewer)
+            {
+                int_ptr = proc.Handle; //Handle Window :D
+            }
+            return int_ptr; //Returning Result of Handle Window :D
+        }
+        public bool SuspendGameWer()
+        {
+            Process[] process = Process.GetProcessesByName("Alkad.exe");
+            if(process.Length == 1)
+            {
+                NtSuspendProcess(GetHandleGameWerProcess()); //Fully Suspending Alkad AntiCheat :D
+                return true; //Fully Suspending of This AntiCheat :D
+            }
+            else
+            {
+                return false; //Returning If Alkad has not Suspended :D
+            }
+        }
+    }
+}
