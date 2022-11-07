69ffc5078e953e55c522d626b9dd36ab3466572d
GingerLutterium <117631451+GingerLutterium@users.noreply.github.com>
2022-11-07 13:25:29 +0200

    GameWerSuspenderLib(C# Lib with Custom DLLImport)

---
+++ CSharp/WithDllImport/GameWerSuspenderLibCSharp-WithDLLImport/SuspendGameWer.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameWerSuspenderLibCSharp_WithDLLImport
+{
+
+    public class GameWerSuspender
+    {
+        [DllImport("GameWerSuspenderLibCpp.dll")]
+        public static extern bool FindGameWerProcess();
+
+        public void GameWerSuspend()
+        {
+            if (FindGameWerProcess())
+            {
+                File.WriteAllText(@"C:\Temp\Suspend.bin", "[+] GameWer Is Suspended");
+            }
+            else
+            {
+                File.WriteAllText(@"C:\Temp\ErrorSuspend.bin", "[-] GameWer Is Not Suspended");
+                Process.Start("notepad", @"C:\Temp\ErrorSuspend.bin");
+                Environment.Exit(1243);
+            }
+        }
+    }
+}
